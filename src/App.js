712ecd1d45f884c5d63f5309bc7430da1b55db6c
atharv656 <atharv.v.agashe.25@dartmouth.edu>
2023-04-07 23:01:04 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="px-10 py-10 h-24" src="/NytroLogo.png"></img>
+      <h1 className="text-3xl py-20">
+        Site coming soon!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
